115da4ab407148745dd9e05e96b5e4661735013d
Thomas Calmant <thomas.calmant@gmail.com>
2021-01-05 20:55:20 +0100

    JS Code re-format

--- resources/espeasy.js
+++ resources/espeasy.js
@@ -1,40 +1,50 @@
 var http = require('http'),
-    url = require("url"),
-    request = require('request'),
-    ipaddr = process.argv[2],
-    urlJeedom = process.argv[3],
-    loglevel = process.argv[4] || 1000; // Default no log at all
+  url = require("url"),
+  request = require('request'),
+  ipaddr = process.argv[2],
+  urlJeedom = process.argv[3],
+  loglevel = process.argv[4] || 1000; // Default no log at all
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 function answer(req, res) {
-    var ipString = req.connection.remoteAddress;
-    var decodeUrl = decodeURIComponent(req.url).replace(/[/]/g,"");
-    if (loglevel <= 200) {console.log("We have got a request for " + decodeUrl + " from " + ipString);} // INFO
-    urlj = urlJeedom + "&" + decodeUrl+ "&ip=" + ipString;
-    if (loglevel <= 100) {console.log("Calling Jeedom " + urlj);} // DEBUG
-  	request({
-  		url: urlj,
-  		method: 'PUT',
-  	},
-  	function (error, response, body) {
-  		if (!error && response.statusCode == 200) {
-  			if (loglevel <= 100) {console.log((new Date()) + "Got response Value: " + response.statusCode);} // DEBUG
-  		}else{
-  			console.log((new Date()) + " - Error : "  + error );
-  		}
-  	});
+  var ipString = req.connection.remoteAddress;
+  var decodeUrl = decodeURIComponent(req.url).replace(/[/]/g, "");
+  if (loglevel <= 200) {
+    // INFO
+    console.log("We have got a request for " + decodeUrl + " from " + ipString);
+  }
 
-    // HTTP response header - the content will be HTML MIME type
-    res.writeHead(200, {'Content-Type': 'text/html'});
+  var urlj = urlJeedom + "&ip=" + ipString + "&" + decodeUrl;
+  if (loglevel <= 100) {
+    // DEBUG
+    console.log("Calling Jeedom " + urlj);
+  }
+  request({
+    url: urlj,
+    method: 'PUT',
+  },
+    function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        if (loglevel <= 100) {
+          // DEBUG
+          console.log((new Date()) + "Got response Value: " + response.statusCode);
+        }
+      } else {
+        console.log((new Date()) + " - Error: " + response.statusCode + " - " + error);
+      }
+    });
 
-    // Write out the HTTP response body
-    res.write('<html><body>' +
-    '<h1>Jeedom receive<h1>'+
+  // HTTP response header - the content will be HTML MIME type
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+
+  // Write out the HTTP response body
+  res.write('<html><body>' +
+    '<h1>Jeedom receive<h1>' +
     '</body></html>');
 
-    // End of HTTP response
-    res.end();
+  // End of HTTP response
+  res.end();
 }
 
 /************************/
