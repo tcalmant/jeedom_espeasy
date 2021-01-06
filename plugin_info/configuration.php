387e7437a6b54e97dcbdc2aa09401376040345e5
Thomas Calmant <thomas.calmant@gmail.com>
2021-01-06 18:40:08 +0000

    Updated ESP publish template

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -47,7 +47,7 @@ if (!isConnect()) {
 
       <div class="form-group">
         <label class="col-lg-4 control-label">{{Publish template à saisir dans ESPeasy (onglet tools, puis bouton advanced)}} :</label>
-        <div class="col-lg-4">device=%sysname%&taskid=%id%&cmd=%valname%&value=%value%
+        <div class="col-lg-4"><code>?name=%sysname%&unitId=%unit%&ip=%ip%&task=%tskname%&valuename=%valname%&value=%value%</code>
         </div>
       </div>
 
